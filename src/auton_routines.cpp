b43fe9d58f19ff0073239123d49466cf4bdb35ca
MBLee2 <masonbyrnelee@icloud.com>
2024-09-07 16:04:07 -0700

    Skills 3rd corner fixes

--- src/auton_routines.cpp
+++ src/auton_routines.cpp
@@ -214,7 +214,7 @@ void auton_60s_skills_1() {
   lemlib::AngularDirection::CCW_COUNTERCLOCKWISE, .maxSpeed = max_speed1});
 
   chassis.moveToPoint(-64, 0, 2000, {.maxSpeed = max_speed});
-  pros::Task([=] {
+  pros::Task save_basket1([=] {
       while(intake_dist.get() > 20){
           pros::delay(10);
       }
@@ -334,9 +334,7 @@ void auton_60s_skills_1() {
 
   printPosition((char*)"after turn");
 
-   pros::Task lift_down356([=] { moveLift(300); });
-
-   intake.move(127);
+  intake.move(127);
   printPosition((char*)"intake started");
   // chassis.moveToPose(24, 24, 135, 2000);
   // chassis.moveToPoint(24, 24, 2000, {.maxSpeed = max_speed});
@@ -368,13 +366,13 @@ printPosition((char*)"after reset", true);
 
 
 
-  chassis.moveToPoint(48, 24, 2000, {.maxSpeed = max_speed});
+  chassis.moveToPoint(48, 27, 2000, {.maxSpeed = max_speed});
   chassis.waitUntilDone();
   printPosition((char*)"second ring");
   chassis.turnToHeading(0, 2000, {.maxSpeed = max_speed1});
   chassis.waitUntilDone();
   pros::delay(50);
-printPosition((char*)"after turn");  
+  printPosition((char*)"after turn");  
 
   pros::delay(50);
   chassis.moveToPoint(48, 50, 2000);
@@ -386,19 +384,26 @@ printPosition((char*)"after turn");
     chassis.waitUntilDone();
   chassis.turnToHeading(15, 2000, {.maxSpeed = max_speed1});
     chassis.waitUntilDone();
-  mogo_rush.extend();
   pros::delay(50);
-  chassis.moveToPoint(90, 85, 2000, {.maxSpeed = max_speed});
+  chassis.moveToPoint(59, 61, 2000, {.maxSpeed = max_speed});
+  pros::Task save_basket2([=] {
+      while(intake_dist.get() > 20){
+          pros::delay(10);
+      }
+      intake.move(0);
+  });
+  moveLift(300);
+  chassis.waitUntil(33);
+  mogo_rush.extend();
     chassis.waitUntilDone();
     currentPose = chassis.getPose();
-  chassis.turnToHeading(currentPose.theta-160, 2000);
+  chassis.turnToHeading(currentPose.theta-160, 2000, {.maxSpeed = max_speed1});
   chassis.waitUntilDone();
   pros::delay(200);
   mogo_clamp.set_value(false);
   pros::delay(300);
-  chassis.moveToPoint(95, 95, 500, {.forwards = false, .maxSpeed = max_speed});
+  chassis.moveToPoint(70, 70, 500, {.forwards = false, .maxSpeed = max_speed});
   intake.move(-50);
-  mogo_clamp.set_value(false);
   pros::delay(250);
   intake.move(0);
   /*
