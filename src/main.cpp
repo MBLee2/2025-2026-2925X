f1a6b6ff6ad674685262b3d37b10a61ee16321c6
Jay-Thpr <ja.thapar@gmail.com>
2024-10-20 02:00:36 -0700

    Test Commit

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 #include "hal.h"
 
 /**
- * Runs initialization code. This occurs as soon as the program is started. 
+ * Runs initialization code. This occurs as soon as the program is started.
  *
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
