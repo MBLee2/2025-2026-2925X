3c00958a6e3cfe264fee8bfd69ccf22dbd1d9a12
rudra pal <rudrashantanupal@gmail.com>
2025-05-05 09:27:38 -0500

    skills path

--- src/auton_routines.cpp
+++ src/auton_routines.cpp
@@ -1670,7 +1670,7 @@ void auton_60s_skills_1() {
   COLOR = true;
   stopSorting();
   int time = pros::millis();
-  float speed = 130;
+  float speed = 70;
   int speed1 = (int) speed;
   int temp = 0;
   chassis.setPose(0,-61.3,0);
@@ -1690,28 +1690,33 @@ void auton_60s_skills_1() {
   closeClamp();
 
   printPositionV2((char *) "Goal clamp"); 
-  chassis.turnToPoint(-24, -24, 1000, {.maxSpeed = speed1,.minSpeed = 3,.earlyExitRange = 0.25}, false);
+  chassisWithGoal.turnToPoint(-24, -24, 1000, {.maxSpeed = speed1,.minSpeed = 3,.earlyExitRange = 0.25}, false);
   spinIntake(127);
-  chassis.moveToPoint(-24, -24, 1500, {.maxSpeed = speed,.minSpeed = 3,.earlyExitRange = 0.25}, false);
+  chassisWithGoal.moveToPoint(-24, -24, 1500, {.maxSpeed = speed,.minSpeed = 3,.earlyExitRange = 0.25}, false);
   printPositionV2((char *) "1st ring");
-  chassis.turnToPoint(-50, 24, 1000,{.maxSpeed = speed1,.minSpeed = 3,.earlyExitRange = 0.25});
-  chassis.moveToPoint(-50, 24, 3500, {.maxSpeed = speed,.minSpeed = 3,.earlyExitRange = 0.25});
-  chassis.moveToPoint(-46, 0, 3500, {.forwards=false,.maxSpeed = speed});
+
+  chassisWithGoal.turnToPoint(-50, 24, 1000,{.maxSpeed = speed1,.minSpeed = 3,.earlyExitRange = 0.25});
+  chassisWithGoal.moveToPoint(-50, 24, 3500, {.maxSpeed = speed,.minSpeed = 3,.earlyExitRange = 0.25});
+  printPositionV2((char *) "LadyBrown Ring");
+  chassisWithGoal.turnToHeading(0, 1000,{.maxSpeed = speed1,.minSpeed = 3,.earlyExitRange = 0.25});
   liftPickup();
+  chassis.moveToPoint(-50, 0, 3500, {.forwards=false,.maxSpeed = speed});
   chassis.turnToHeading(270, 1000);
-  /*stopIntake();
-  liftUpWallStake();
-  spinIntake(127);  
+  stopIntake();
+  pros::Task scoreWallStake1 (liftUpWallStake);
+  spinIntake(127);
   chassis.moveToPoint(-65, 0, 1000);
-  //hit wall stake
-  chassis.moveToPoint(-48, 0, 1500, {.forwards=false,.maxSpeed = speed,.minSpeed = 3,.earlyExitRange = 0.25}, false);
-  moveLiftToPos(0);
-  chassis.turnToPoint(-48, -24, 1000, {.maxSpeed = speed1,.minSpeed = 3,.earlyExitRange = 0.25});
-  chassis.moveToPoint(-50, -22, 1500, {.maxSpeed = speed,.minSpeed = 3,.earlyExitRange = 0.25}, false);
+  printPositionV2((char *) "Score Wall stake");
+  moveLiftToPos(550);
+  pros::delay(1000);
   printPositionV2((char *) "2nd ring");
-  chassis.turnToPoint(-50, -48, 1500, {.maxSpeed = speed1,.minSpeed = 3,.earlyExitRange = 0.25});
-  chassis.moveToPoint(-50, -48, 2000, {.maxSpeed = 60,}, false);
-  pros::delay(200);
+
+  chassis.moveToPoint(-48, 0, 1500, {.forwards=false,.maxSpeed = speed,.minSpeed = 3,.earlyExitRange = 0.25});
+  moveToReset(100);
+  chassis.turnToPoint(-48, -24, 1000, {.maxSpeed = speed1,.minSpeed = 3,.earlyExitRange = 0.25});
+  spinIntake(127);  
+  chassis.moveToPoint(-50, -24, 1500, {.maxSpeed = speed,.minSpeed = 3,.earlyExitRange = 0.25}, false);
+  printPositionV2((char *) "3rd ring");
   chassis.moveToPoint(-50, -57, 2000, {.maxSpeed = 60,.minSpeed = 3,.earlyExitRange = 0.25}, false);
   printPositionV2((char *) "4th and 5th ring");
   chassis.turnToPoint(-42, -48, 1000, {.forwards = false, .maxSpeed = speed1,.minSpeed = 3,.earlyExitRange = 0.25});
@@ -1726,7 +1731,7 @@ void auton_60s_skills_1() {
   spinIntake(-127);
   printPositionV2((char *) "Goal drop");
   pros::delay(300);
-
+  /*
   chassis.moveToPoint(-57, -48, 1000, {.maxSpeed = speed,.minSpeed = 3,.earlyExitRange = 0.25});
   chassis.turnToHeading(0, 1000, {.maxSpeed = speed1}, false);
   pros::delay(50);
@@ -1810,7 +1815,7 @@ void auton_60s_skills_1() {
   chassis.moveToPoint(28, -60, 1000,{});
   chassis.turnToPoint(72, -65, 1000);
   chassis.moveToPoint(72, -65, 1800);
-  chassis.moveToPoint(30, -59, 1800,{.forwards = false}); /*/
+  chassis.moveToPoint(30, -59, 1800,{.forwards = false}); //*/
 
   master.clear_line(0);
   temp = pros::millis();
@@ -1818,6 +1823,8 @@ void auton_60s_skills_1() {
     master.print(0, 0, "Time: %d", (temp-time));
   }
 } // end auton_60s_skills_1()
+
+
 // Auton skills number 2 fully done
 void auton_60s_skills_2() {
     COLOR = true;
